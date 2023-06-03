47928237cc86224bbac7ce3e02ad0e0d6c106f96
Rajan <rajan.k@krishworks.com>
2023-06-04 02:20:40 +0530

    working on favorite

--- index.js
+++ index.js
@@ -10,30 +10,68 @@ const debug = true;
 function generateMD5Hash(value) {
   return CryptoJS.MD5(value).toString();
 }
+// Function to show the loading screen
+function showLoadingScreen() {
+  if (debug) {
+    console.log("showLoadingScreen Start");
+  }
+  document.getElementById("loading-screen").style.display = "block";
+  document.getElementById("pagination").style.display = "none";
+}
 
+// Function to hide the loading screen
+function hideLoadingScreen() {
+  if (debug) {
+    console.log("hideLoadingScreen Start");
+    console.log(document.getElementById("loading-screen"));
+  }
+  document.getElementById("loading-screen").style.display = "none";
+  document.getElementById("pagination").style.display = "flex";
+}
 // Function to call the Marvel API
 function callMarvelAPI() {
+  showLoadingScreen();
   const ts = new Date().getTime().toString();
   const hash = generateMD5Hash(ts + privateKey + publicKey);
   const apiUrl = `https://gateway.marvel.com:443/v1/public/characters?apikey=${publicKey}&ts=${ts}&hash=${hash}&limit=${limit}&offset=${offset}`;
   fetch(apiUrl)
     .then((response) => response.json())
-      .then((data) => {
-        console.log(data)
+    .then((data) => {
+      if (debug) {
+        console.log(data);
+      }
+      hideLoadingScreen();
       // Process the API response data here
-        cardContainer.innerHTML = "";
-        var superHeroArray = data.data.results;
-        totalElements = data.data.totals;
-        for (let i = 0; i < superHeroArray.length; i++) {
-          var card = cardTemplate.cloneNode(true); // Clone the card template
-            card.id = "card_" + superHeroArray[i]; // Set a unique ID for each card
-            var imgElement = card.querySelector(".movie-img img");
-            imgElement.src = superHeroArray[i].thumbnail.path + "." + superHeroArray[i].thumbnail.extension;
-            var imgElement = card.querySelector(".movie-info span");
-            imgElement.innerHTML =
-              superHeroArray[i].name;
-          cardContainer.appendChild(card);
-        }
+      cardContainer.innerHTML = "";
+      var superHeroArray = data.data.results;
+      totalElements = data.data.total;
+      for (let i = 0; i < superHeroArray.length; i++) {
+        var card = cardTemplate.cloneNode(true); // Clone the card template
+        card.id = "card_" + superHeroArray[i]; // Set a unique ID for each card
+        var imgElement = card.querySelector(".card-img img");
+        imgElement.src = superHeroArray[i].thumbnail.path + "." + superHeroArray[i].thumbnail.extension;
+        var imgElement = card.querySelector(".card-info span");
+        imgElement.innerHTML =
+          superHeroArray[i].name;
+        cardContainer.appendChild(card);
+      }
+      document.getElementById("current-page").innerText = `${data.data.offset + 1
+        } - ${data.data.offset + data.data.count} of ${totalElements} `;
+      if (offset === 0) {
+        let previousButton = document.getElementById("previous-button");
+        previousButton.classList.add("disabled");
+      } else {
+        let previousButton =
+          document.getElementById("previous-button");
+        previousButton.classList.remove("disabled");
+      }
+      if (data.data.offset + data.data.count >= totalElements) {
+        let nextButton = document.getElementById("next-button");
+        nextButton.classList.add("disabled");
+      } else {
+        let nextButton = document.getElementById("next-button");
+        nextButton.classList.remove("disabled");
+      }
     })
     .catch((error) => {
       // Handle any errors that occur during the API call
@@ -46,24 +84,27 @@ function callMarvelAPI() {
 function moveNextPage() {
   if (debug) {
     console.log("moveNextPage function Start");
+    console.log("offset", offset);
+    console.log("totalElements", totalElements);
   }
   if (offset < totalElements) {
     offset += limit;
     callMarvelAPI();
-  
   }
 }
-  //function to move previous page
+//function to move previous page
 function movePreviousPage() {
   if (debug) {
     console.log("movePreviousPage function Start");
+    console.log("offset", offset);
+    console.log("totalElements", totalElements);
   }
   if (offset > 0) {
     offset -= limit;
     callMarvelAPI();
   }
 }
-  // Function to add click event listeners to the buttons
+// Function to add click event listeners to the buttons
 (function addButtonEventListeners() {
   var button = document.getElementById("next-page");
   button.addEventListener("click", moveNextPage);
